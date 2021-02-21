ad8f497dc2830471ee1a8931116f0d1bcc8bcace
Fedeee9 <federicodalmonte@live.it>
2021-02-21 10:46:03 +0100

    update

---
+++ evaluate_bounding_box.py
@@ -0,0 +1,27 @@
+from preprocess_data import data_generator, load_labels, load_test_dataset
+import config
+import os
+from tensorflow.keras.models import load_model
+from preprocess_data_bb import data_generator
+import tensorflow as tf
+import lesion_detector_model
+
+
+def evaluate(model_evaluate):
+    test_data_gen = data_generator(config.test_dir_bb, config.test_csv, config.batch_size)
+
+    results = model_evaluate.evaluate(x=test_data_gen, verbose=1, return_dict=True)
+    loss = results['loss']
+    accuracy = results['accuracy']
+    print('\nMODEL: {}\nACCURACY: {:.2f}%'.format(config.model_detector, accuracy * 100))
+    print('\nMODEL: {}\nLOSS: {:.2f}%'.format(config.model_detector, loss))
+
+
+if __name__ == "__main__":
+    if os.path.isfile(config.model_detector):
+        print("Loading object detector...")
+        model = load_model(config.model_detector)
+        # model.compile(loss='mse', metrics=['accuracy'])
+        evaluate(model)
+    else:
+        print("Model not found in {}".format(config.model_detector))
